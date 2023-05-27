bfb6bdf632b6ac0cd26e6c5fd9c4cc8494113348
masariuman <masariuman@gmail.com>
2023-05-27 22:21:51 +0700

    eror dikit

--- struct/data/artikel.go
+++ struct/data/artikel.go
@@ -4,10 +4,10 @@ import "time"
 
 type Artikel struct {
 	Id        uint      `gorm:"primaryKey"`
-	Judul     string    `gorm:"type:varchar(255)" 	json:"judul"`
-	Deskripsi string    `gorm:"type:text" 					json:"deskripsi"`
-	Isi       string    `gorm:"type:text" 					json:"isi"`
-	Penulis   string    `gorm:"type:varchar(255)" 	json:"penulis"`
+	Judul     string    `gorm:"type:varchar(255)" json:"judul"`
+	Deskripsi string    `gorm:"type:text" json:"deskripsi"`
+	Isi       string    `gorm:"type:text" json:"isi"`
+	Penulis   string    `gorm:"type:varchar(255)" json:"penulis"`
 	CreatedAt time.Time `gorm:"type:datetime"`
 	UpdatedAt time.Time `gorm:"type:datetime"`
 }
